7a59cce9062dbc2b0b8d109bb1c5c9c22be96fdb
02Ian <37692875+02Ian@users.noreply.github.com>
2019-01-31 18:11:10 +0530

    Add files via upload

---
+++ intro/ifstatement.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+        <body>
+            <?php
+                $isMale = true;
+                  if($isMale)
+                  {
+                    echo "YOU ARE MALE";
+                  }
+                  else {
+                    echo "YOU ARE NOT MALE";
+                  }
+            ?>
+            <?php
+              /*  function getMax($num1 , $num2)
+                {
+                  if($num1 > $num) {
+                    return $num1;
+                  }
+                }* /
+              ?>
+        </body>
+</html>
