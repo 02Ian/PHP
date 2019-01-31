77d7630f7bcc686af54f03305c1af740ea43689d
02Ian <37692875+02Ian@users.noreply.github.com>
2019-01-31 14:42:00 +0530

    Add files via upload

---
+++ intro/datatype.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>php learn</title>
+  </head>
+  <body>
+    <?php
+        $phrase = "Sky never ends";
+        $age = 21;
+        $CGPA = 6.85;
+        $isMale = true;
+
+        echo "<h1>$age</h1>";
+        echo "<h2>$phrase</h2>";
+        echo "<h1>$CGPA</h1>";    
+     ?>
+  </body>
+</html>
